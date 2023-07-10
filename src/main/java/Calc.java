e3ae8e88e942641d376be41637438efecca5e467
VKuzevanov <ks-94@bk.ru>
2023-07-11 00:50:07 +0500

    add OOP + UTests

--- src/main/java/Calc.java
+++ src/main/java/Calc.java
@@ -5,34 +5,46 @@
 import java.util.Scanner;
 
 public class Calc {
-    public void StartCal(){
-        double num1;
-        double num2;
-        double ans;
-        char op;
+    public void StartCal() {
 
         Scanner in = new Scanner(System.in);
         System.out.print("Enter two numbers: ");
-        num1 = in.nextDouble();
-        num2 = in.nextDouble();
+        double num1 = in.nextDouble();
+        double num2 = in.nextDouble();
         System.out.print("\nChoose an operator(+,-,*,/): ");
-        op = in.next().charAt(0);
-        in.close();
+        char op = in.next().charAt(0);
 
-        switch(op) {
-            case '+': ans = num1 + num2;
-                break;
-            case '-': ans = num1 - num2;
-                break;
-            case '*': ans = num1 * num2;
-                break;
-            case '/': ans = num1 / num2;
-                break;
-            default:  System.err.printf("Wrong op");
-                return;
+        if (op != '+' && op != '-' && op !='*' && op != '/'){
+            System.err.printf("Wrong op");
+            return;
         }
 
-        System.out.print("The result of operation:\n");
-        System.out.printf(num1 + " " + op + " " + num2 + " = " + String.format("%.4f", ans));
+        double result = calculate(num1, num2, op);
+        System.out.print("\nThe result of operation:\n");
+        System.out.printf(num1 + " " + op + " " + num2 + " = " + String.format("%.4f", result));
+
+        in.close();
     }
-}
+        public double calculate(double num1, double num2, char op) {
+
+        double ans = 0;
+
+            switch (op) {
+                case '+':
+                    ans = num1 + num2;
+                    break;
+                case '-':
+                    ans = num1 - num2;
+                    break;
+                case '*':
+                    ans = num1 * num2;
+                    break;
+                case '/':
+                    ans = num1 / num2;
+                    break;
+                default:
+            }
+
+            return ans;
+        }
+    }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,6 @@
 /**
  * @author Вячеслав Кузеванов
  * @see Calc
- * @see Words
  */
 
 import java.util.Scanner;
@@ -9,23 +8,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
 
-    int ch;
-    System.out.print("Enter number of task: (1 - calculator, 2 - string array)\n");
-    Scanner in = new Scanner(System.in);
-    ch = in.next().charAt(0);
-
-    if (ch == '1') {
-
         Calc calc = new Calc();
         calc.StartCal();
-
-        } else if (ch == '2') {
-
-            Words words = new Words();
-            words.WordsStart();
-            } else {
-            System.err.println("Wrong number of task");
-        }
-    in.close();
     }
 }
