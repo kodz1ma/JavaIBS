f892f1679ae126e90ac6c177da3fe348b3e4e8f2
VKuzevanov <ks-94@bk.ru>
2023-07-14 17:29:57 +0500

    And UnitTests

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,31 +1,68 @@
 /**
  * @author Вячеслав Кузеванов
- * @see Calc
- * @see Words
  */
 
-import java.util.Scanner;
+import java.io.*;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class Main {
     public static void main(String[] args) {
+        CountWords("files/words.txt");
+    }
 
-    int ch;
-    System.out.print("Enter number of task: (1 - calculator, 2 - string array)\n");
-    Scanner in = new Scanner(System.in);
-    ch = in.next().charAt(0);
+    public static void CountWords(String filepath) {
 
-    if (ch == '1') {
+        Map<String, Integer> wordsList = new TreeMap<>();
+        String text = TextFile(filepath);
 
-        Calc calc = new Calc();
-        calc.StartCal();
 
-        } else if (ch == '2') {
-
-            Words words = new Words();
-            words.WordsStart();
-            } else {
-            System.err.println("Wrong number of task");
+        String[] words = text.split(" ");
+        for (String word : words) {
+            if (!wordsList.containsKey(word)) wordsList.put(word, 1);
+            else wordsList.put(word, wordsList.get(word) + 1);
         }
-    in.close();
+        System.out.println("WORDS---Numbers_of_repetitions");
+
+        String key = null;
+        int value = 0;
+        int maxCount = 0;
+        for (Map.Entry<String, Integer> entry : wordsList.entrySet()) {
+            key = entry.getKey();
+            value = entry.getValue();
+            if (value > maxCount) maxCount = value;
+            System.out.println(String.format("%s---%d", key, value));
+        }
+
+        for (Map.Entry<String, Integer> entry : wordsList.entrySet()) {
+            key = entry.getKey();
+            value = entry.getValue();
+            if (value == maxCount)
+                System.out.println(String.format("\nWord '%s' in file: %d times", key, value));
+        }
+    }
+
+    public static String TextFile(String filepath) {
+        File file = new File(filepath);
+        String text = null;
+        try (FileReader reader = new FileReader(file)) {
+
+            char[] buf = new char[256];
+            int c;
+            while ((c = reader.read(buf)) > 0) {
+                if (c < 256) {
+                    buf = Arrays.copyOf(buf, c);
+                }
+            }
+            text = String.valueOf(buf);
+
+        } catch (FileNotFoundException e) {
+            System.err.println("File not found!!!");
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return text;
     }
 }
