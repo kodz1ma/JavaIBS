fab4804b3819f4a29f193b87d47d1538f2d22968
VKuzevanov <ks-94@bk.ru>
2023-07-14 13:22:40 +0500

    And another exceptions

--- src/main/java/Calc.java
+++ src/main/java/Calc.java
@@ -2,10 +2,6 @@
  * @author Вячсеслав Кузеванов
  */
 
-import java.util.InputMismatchException;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 public class Calc {
 
     public double StartCal(String num1, String num2, char op) {
@@ -13,51 +9,46 @@ public class Calc {
         double ans = 0;
 
         try {
-            Pattern pattern = Pattern.compile("[a-zA-Z]");
-            Matcher matcher = pattern.matcher(num1);
-            Matcher matcher2 = pattern.matcher(num2);
+            Double.parseDouble(num1.replace(",", "."));
+            Double.parseDouble(num2.replace(",", "."));
 
-            if (matcher.find() || matcher2.find()) {
-                throw new NumberFormatException("Wrong numbers!!!");
+            if (String.valueOf(op).matches("[^+-/*]")) {
+                throw new Exception("Wrong OP!!!");
             }
-        } catch (NumberFormatException e){
-            System.out.println(e.getMessage());
-            return ans;
+        } catch (NumberFormatException e) {
+            System.err.println("Numbers contains wrong simbols!!!\n");
+        } catch (Exception e){
+            System.err.println(e.getMessage());
         }
 
-        double num1d = Double.parseDouble(num1.replace(",","."));
-        double num2d = Double.parseDouble(num2.replace(",","."));
+        double num1d = Double.parseDouble(num1.replace(",", "."));
+        double num2d = Double.parseDouble(num2.replace(",", "."));
 
-            switch (op) {
-                case '+':
-                    ans = num1d + num2d;
-                    break;
-                case '-':
-                    ans = num1d - num2d;
-                    break;
-                case '*':
-                    ans = num1d * num2d;
-                    break;
-                case '/':
-                    ans = num1d / num2d;
-                    break;
-                default:
-            }
+        switch (op) {
+            case '+':
+                ans = num1d + num2d;
+                break;
+            case '-':
+                ans = num1d - num2d;
+                break;
+            case '*':
+                ans = num1d * num2d;
+                break;
+            case '/':
+                ans = num1d / num2d;
+                break;
+            default:
+        }
 
-        try {
-            if (ans == Double.POSITIVE_INFINITY){
+        try{
+            if (ans == Double.POSITIVE_INFINITY) {
                 throw new ArithmeticException("Divided by zero!!!");
             }
-            if (String.valueOf(op).matches("[^+-/*]")){
-                throw new Exception("Wrong operator!!!");
-            }
-        } catch (Exception e){
-            System.out.println(e.getMessage());
+        } catch (ArithmeticException e){
+                    System.err.println(e.getMessage());
         }
 
-
         return ans;
-
     }
 }
 
--- src/test/java/TestCulc.java
+++ src/test/java/TestCulc.java
@@ -1,30 +1,19 @@
 import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 public class TestCulc {
     Calc calc = new Calc();
 
-    @Test
-    public void TestOpPlus(){
-        Assert.assertEquals(calc.calculate(1,2,'+'), 3,0.0001);
-        Assert.assertEquals(calc.calculate(2.22222,2.33333,'+'), 4.5555,0.0001);
-    }
+    @Rule
+    public ExpectedException thrown= ExpectedException.none();
 
     @Test
-    public void TestOpMinus(){
-        Assert.assertEquals(calc.calculate(2,1,'-'), 1,0.0001);
-        Assert.assertEquals(calc.calculate(2.22222,2.11111,'-'), 0.1111,0.0001);
-    }
+    public void TestForException(){
 
-    @Test
-    public void TestOpDivision(){
-        Assert.assertEquals(calc.calculate(3,2,'/'), 1.500,0.0001);
-        Assert.assertEquals(calc.calculate(5.4444,2.2222,'/'), 2.4500,0.0001);
-    }
+        thrown.expect(ArithmeticException.class);
+        calc.StartCal("2","0",'/');
 
-    @Test
-    public void TestOpMultiplication(){
-        Assert.assertEquals(calc.calculate(2,2,'*'), 4,0.0001);
-        Assert.assertEquals(calc.calculate(4.2222,5.3,'*'), 22.3776,0.0001);
     }
 }
